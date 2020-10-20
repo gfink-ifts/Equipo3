e33c291f2649bf3db18742d52a1e19d3fd4a6868
NICO <1548@ifts24.edu.ar>
2020-10-20 17:38:16 -0300

    Botones Form1

--- Equipo3/Equipo3/Form1.Designer.cs
+++ Equipo3/Equipo3/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.btnVentas = new System.Windows.Forms.Button();
+            this.btnProductos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.Location = new System.Drawing.Point(12, 12);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(278, 60);
+            this.btnClientes.TabIndex = 0;
+            this.btnClientes.Text = "CLIENTES";
+            this.btnClientes.UseVisualStyleBackColor = true;
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.Location = new System.Drawing.Point(12, 210);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.Size = new System.Drawing.Size(278, 60);
+            this.btnReportes.TabIndex = 1;
+            this.btnReportes.Text = "REPORTES";
+            this.btnReportes.UseVisualStyleBackColor = true;
+            // 
+            // btnVentas
+            // 
+            this.btnVentas.Location = new System.Drawing.Point(12, 144);
+            this.btnVentas.Name = "btnVentas";
+            this.btnVentas.Size = new System.Drawing.Size(278, 60);
+            this.btnVentas.TabIndex = 2;
+            this.btnVentas.Text = "VENTAS";
+            this.btnVentas.UseVisualStyleBackColor = true;
+            // 
+            // btnProductos
+            // 
+            this.btnProductos.Location = new System.Drawing.Point(12, 78);
+            this.btnProductos.Name = "btnProductos";
+            this.btnProductos.Size = new System.Drawing.Size(278, 60);
+            this.btnProductos.TabIndex = 3;
+            this.btnProductos.Text = "PRODUCTOS";
+            this.btnProductos.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(302, 384);
+            this.Controls.Add(this.btnProductos);
+            this.Controls.Add(this.btnVentas);
+            this.Controls.Add(this.btnReportes);
+            this.Controls.Add(this.btnClientes);
+            this.Name = "Form1";
+            this.Text = "Carnicería";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnClientes;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.Button btnVentas;
+        private System.Windows.Forms.Button btnProductos;
     }
 }
 
