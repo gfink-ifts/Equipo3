5bc9e44870a11c70d0140ae91064bb338ceb5aa7
NICO <1548@ifts24.edu.ar>
2020-10-26 22:06:01 -0300

    datagrid y am.. falta la b

--- Equipo3/Equipo3/CLIENTES.Designer.cs
+++ Equipo3/Equipo3/CLIENTES.Designer.cs
@@ -32,14 +32,28 @@
             this.btnAltaCliente = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.dtgvClientes = new System.Windows.Forms.DataGridView();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.cmbTipoCliente = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txt_CUIT = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvClientes)).BeginInit();
             this.SuspendLayout();
             // 
             // btnCerrar
             // 
-            this.btnCerrar.Location = new System.Drawing.Point(29, 211);
+            this.btnCerrar.Location = new System.Drawing.Point(397, 508);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(196, 23);
+            this.btnCerrar.Size = new System.Drawing.Size(378, 25);
             this.btnCerrar.TabIndex = 1;
             this.btnCerrar.Text = "CERRAR";
             this.btnCerrar.UseVisualStyleBackColor = true;
@@ -47,9 +61,9 @@
             // 
             // btnAltaCliente
             // 
-            this.btnAltaCliente.Location = new System.Drawing.Point(29, 12);
+            this.btnAltaCliente.Location = new System.Drawing.Point(677, 426);
             this.btnAltaCliente.Name = "btnAltaCliente";
-            this.btnAltaCliente.Size = new System.Drawing.Size(90, 90);
+            this.btnAltaCliente.Size = new System.Drawing.Size(98, 59);
             this.btnAltaCliente.TabIndex = 2;
             this.btnAltaCliente.Text = "ALTA";
             this.btnAltaCliente.UseVisualStyleBackColor = true;
@@ -57,44 +71,181 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(135, 12);
+            this.button2.Location = new System.Drawing.Point(446, 418);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 90);
+            this.button2.Size = new System.Drawing.Size(98, 59);
             this.button2.TabIndex = 3;
             this.button2.Text = "BAJA";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(29, 115);
+            this.button3.Location = new System.Drawing.Point(446, 312);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(90, 90);
+            this.button3.Size = new System.Drawing.Size(98, 59);
             this.button3.TabIndex = 4;
             this.button3.Text = "MODIFICACION";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // dtgvClientes
             // 
-            this.button4.Location = new System.Drawing.Point(135, 115);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(90, 90);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "VER TABLA";
-            this.button4.UseVisualStyleBackColor = true;
+            this.dtgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvClientes.Location = new System.Drawing.Point(12, 12);
+            this.dtgvClientes.Name = "dtgvClientes";
+            this.dtgvClientes.Size = new System.Drawing.Size(763, 269);
+            this.dtgvClientes.TabIndex = 6;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(126, 472);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(114, 20);
+            this.txtEmail.TabIndex = 39;
+            // 
+            // cmbTipoCliente
+            // 
+            this.cmbTipoCliente.FormattingEnabled = true;
+            this.cmbTipoCliente.Items.AddRange(new object[] {
+            "EMPRESA",
+            "PARTICULAR"});
+            this.cmbTipoCliente.Location = new System.Drawing.Point(126, 298);
+            this.cmbTipoCliente.Name = "cmbTipoCliente";
+            this.cmbTipoCliente.Size = new System.Drawing.Size(114, 21);
+            this.cmbTipoCliente.TabIndex = 38;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 306);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "Tipo de Cliente:";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(126, 438);
+            this.txtTelefono.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(114, 20);
+            this.txtTelefono.TabIndex = 36;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(126, 403);
+            this.txtDireccion.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(114, 20);
+            this.txtDireccion.TabIndex = 35;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(126, 368);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(114, 20);
+            this.txtNombre.TabIndex = 34;
+            // 
+            // txt_CUIT
+            // 
+            this.txt_CUIT.Location = new System.Drawing.Point(126, 335);
+            this.txt_CUIT.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_CUIT.Name = "txt_CUIT";
+            this.txt_CUIT.Size = new System.Drawing.Size(114, 20);
+            this.txt_CUIT.TabIndex = 33;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(67, 472);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "E-mail:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 438);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Teléfono:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 403);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Dirección:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 368);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Nombre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 335);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "CUIT:";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(12, 508);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(378, 25);
+            this.btnGuardar.TabIndex = 40;
+            this.btnGuardar.Text = "GUARDAR";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // CLIENTES
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(266, 255);
-            this.Controls.Add(this.button4);
+            this.ClientSize = new System.Drawing.Size(787, 545);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.cmbTipoCliente);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txt_CUIT);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtgvClientes);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btnAltaCliente);
             this.Controls.Add(this.btnCerrar);
             this.Name = "CLIENTES";
             this.Text = "CLIENTES";
+            this.Load += new System.EventHandler(this.CLIENTES_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvClientes)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -103,6 +254,19 @@
         private System.Windows.Forms.Button btnAltaCliente;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dtgvClientes;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.ComboBox cmbTipoCliente;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txt_CUIT;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGuardar;
     }
 }
--- Equipo3/Equipo3/CLIENTES.cs
+++ Equipo3/Equipo3/CLIENTES.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace Equipo3
 {
     public partial class CLIENTES : Form
     {
+        string cadenaConex = @"data source=.\SQLEXPRESS; initial catalog = CARNICERIA; integrated security= SSPI";
+        SqlConnection cn;
+        private bool edicion = false;
         public CLIENTES()
         {
             InitializeComponent();
@@ -26,6 +31,103 @@ namespace Equipo3
         {
             ALTA_CLIENTE formAltaCliente = new ALTA_CLIENTE();
             formAltaCliente.ShowDialog();
+            mostrarClientes();
+        }
+
+        private void CLIENTES_Load(object sender, EventArgs e)
+        {
+            mostrarClientes();
+        }
+
+        private void mostrarClientes()
+        {
+            cn = new SqlConnection(cadenaConex);
+            SqlDataAdapter da;
+            DataTable dt = new DataTable();
+            string query = "select * from cliente";
+            cn.Open();
+            da = new SqlDataAdapter(query, cn);
+            da.Fill(dt);
+            cn.Close();
+            dtgvClientes.DataSource = dt;
+        }
+
+        private void vaciarCampos()
+        {
+            foreach (Control variable in this.Controls)
+            {
+                if (variable is TextBox)
+                {
+                    (variable as TextBox).Clear();
+                }
+                if (variable is ComboBox)
+                {
+                    (variable as ComboBox).Items.Clear();
+                }
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string tipo_cliente, CUIT, nombre, direccion, telefono, email;
+            tipo_cliente = cmbTipoCliente.Text;
+            CUIT = txt_CUIT.Text;
+            nombre = txtNombre.Text;
+            direccion = txtDireccion.Text;
+            telefono = txtTelefono.Text;
+            email = txtEmail.Text;
+
+            if (edicion == false) {
+                string cmd = "insert into cliente (tipo_cliente,cuit,nombre,direccion,telefono,email)" + "values ( @tipo_cliente, @CUIT, @nombre, @direccion, @telefono, @email )";
+                SqlCommand comando = new SqlCommand(cmd, cn);
+                comando.Parameters.AddWithValue("@tipo_cliente", tipo_cliente);
+                comando.Parameters.AddWithValue("@CUIT", CUIT);
+                comando.Parameters.AddWithValue("@nombre", nombre);
+                comando.Parameters.AddWithValue("@direccion", direccion);
+                comando.Parameters.AddWithValue("@telefono", telefono);
+                comando.Parameters.AddWithValue("@email", email);
+                cn.Open();
+                comando.ExecuteNonQuery();
+                comando.Parameters.Clear();
+                cn.Close();
+                MessageBox.Show("Usuario creado correctamente");            
+            }
+            if (edicion == true)
+            {
+                string ID = dtgvClientes.CurrentRow.Cells["id_cliente"].Value.ToString();
+                string cmd = "update cliente set tipo_cliente=@tipo_cliente, cuit=@CUIT, nombre=@nombre, direccion=@direccion, telefono=@telefono, email=@email where id_cliente="+ID;
+                SqlCommand comando = new SqlCommand(cmd, cn);
+                comando.Parameters.AddWithValue("@tipo_cliente", tipo_cliente);
+                comando.Parameters.AddWithValue("@CUIT", CUIT);
+                comando.Parameters.AddWithValue("@nombre", nombre);
+                comando.Parameters.AddWithValue("@direccion", direccion);
+                comando.Parameters.AddWithValue("@telefono", telefono);
+                comando.Parameters.AddWithValue("@email", email);
+                cn.Open();
+                comando.ExecuteNonQuery();
+                comando.Parameters.Clear();
+                cn.Close();
+                MessageBox.Show("Usuario editado correctamente");
+                edicion = false;            
+            }
+            vaciarCampos();
+            mostrarClientes();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dtgvClientes.SelectedRows.Count > 0)
+            {
+                edicion = true;
+                cmbTipoCliente.Text = dtgvClientes.CurrentRow.Cells["tipo_cliente"].Value.ToString();
+                txt_CUIT.Text = dtgvClientes.CurrentRow.Cells["cuit"].Value.ToString();
+                txtNombre.Text = dtgvClientes.CurrentRow.Cells["nombre"].Value.ToString();
+                txtDireccion.Text = dtgvClientes.CurrentRow.Cells["direccion"].Value.ToString();
+                txtTelefono.Text = dtgvClientes.CurrentRow.Cells["telefono"].Value.ToString();
+                txtEmail.Text = dtgvClientes.CurrentRow.Cells["email"].Value.ToString();
+            }
+            else
+                MessageBox.Show("Seleccione una fila por favor.");
         }
     }
 }
